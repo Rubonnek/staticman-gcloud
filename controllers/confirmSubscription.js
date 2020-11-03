95821d6c317ceeb316a25d31a7ce03e6d131d1d1
Michael Harry Scepaniak <michael.scepaniak@milestoneinc.com>
2020-11-03 10:39:08 -0500

    Prevent attackers from creating their own valid encrypted strings and subscribing whoever they want to notifications.
    - Add support for a cryptographic pepper to the JSON config schema.
    - Improve error-handling in the confirmSubscription handler.

--- controllers/confirmSubscription.js
+++ controllers/confirmSubscription.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const path = require('path')
+
+const config = require(path.join(__dirname, '/../config'))
 const RSA = require('../lib/RSA')
 const Staticman = require('../lib/Staticman')
 const sendResponse = require('./sendResponse')
@@ -17,8 +20,21 @@ module.exports = async (req, res, next) => {
 
   let confirmData = null
   try {
+    // If the encrypted string is spoofed, the JSON parse will return null.
     const decryptedDataStr = RSA.decrypt(encryptedDataStr)
     confirmData = JSON.parse(decryptedDataStr)
+
+    /*
+     * Verify the pepper we inserted in the encrypted payload when creating the confirmation email.
+     * More info - https://en.wikipedia.org/wiki/Pepper_(cryptography)
+     * We do this because the "encrypt" endpoint is currently exposed for anyone to hit. As such,
+     * an attacker could easily create their own valid encrypted strings and subscribe whoever they
+     * want. They'd be able to see the expected structure of the payload data simply by looking at
+     * the source code.
+     */
+    if (confirmData === null || confirmData.pepper !== config.get('cryptoPepper')) {
+      throw new Error('Authenticity check failed.')
+    }
 
     const staticman = await new Staticman(req.params)
     staticman.setConfigPath()
@@ -36,9 +52,12 @@ module.exports = async (req, res, next) => {
     })
   } catch (error) {
     console.error(error.stack || error)
-    sendResponse(res, {
-      err: error,
-      redirectError: confirmData.subscribeConfirmRedirectError
-    })
+    let data = {
+      err: error
+    }
+    if (confirmData !== null) {
+      data.redirectError = confirmData.subscribeConfirmRedirectError
+    }
+    sendResponse(res, data)
   }
 }
